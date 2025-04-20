f73d7716bd19ddfb120d5e0084cb1c22a9dd8d4d
Guilherme Barbosa <guilhermecirineu@hotmail.com>
2025-04-20 19:52:27 -0300

    build: initial setup

---
+++ src/main/java/com/example/mijava/MijavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijavaApplication.class, args);
+	}
+
+}
