46df36ba97fd13c65bb23562d794aedd970364a9
KauanSoaress <gbskauan@gmail.com>
2025-05-19 21:08:03 -0300

    feat: adding new classes

---
+++ src/main/java/com/example/mijava/ast/MethodCallExpression.java
@@ -0,0 +1,22 @@
+package com.example.mijava.ast;
+
+import com.example.mijava.visitor.ASTVisitor;
+
+import java.util.List;
+
+public class MethodCallExpression extends Expression {
+    public Expression object;
+    public String methodName;
+    public List<Expression> arguments;
+
+    public MethodCallExpression(Expression object, String methodName, List<Expression> arguments) {
+        this.object = object;
+        this.methodName = methodName;
+        this.arguments = arguments;
+    }
+
+    @Override
+    public <T> T accept(ASTVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
